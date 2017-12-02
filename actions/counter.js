63c81f2198e31c09da5f1cf78574da7e3d4d8331
wangbo <xiyou.wangbo@163.com>
2017-12-02 11:53:40 +0800

    init code

---
+++ actions/counter.js
@@ -0,0 +1,26 @@
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+//导出加一的方法
+export function increment() {
+  return {
+    type: INCREMENT_COUNTER
+  }
+}
+//导出减一的方法
+export function decrement() {
+  return {
+    type: DECREMENT_COUNTER
+  }
+}
+
+//导出一个方法,包含一个默认参数delay,返回一个方法,一秒后加一
+export function incrementAsync(delay = 1000) {
+//返回值为一个入参为dispatch的函数,dispatch可以随时执行
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(increment())
+    }, delay)
+  }
+}
+
+//这些方法都导出,在其他文件导入时候,使用import * as actions 就可以生成一个actions对象包含所有的export
